dbd35f0d717f9024411605523643535d854b6145
jbrasko <jbrasko0508@gmail.com>
2021-02-13 17:47:10 -0600

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,22 +20,22 @@ Preset.create(
     delayDial: 0,
     tremSwitch: false,
     tremFreq: 0,
-    chorSwitch: false,
+    chorSwitch: true,
     synth_id: 1
 )
 
 Preset.create(
     name: "Smooth",
-    waveTypeButton: 1,
+    waveTypeButton: 0,
     filterDial: 2000,
     attack: 0.5,
-    decay: 0.4,
-    sustain: 0.7,
-    release: 0.3,
+    decay: 1.5,
+    sustain: 1.4,
+    release: 2,
     reverbDial: 0.5,
     delayDial: 0,
-    tremSwitch: false,
-    tremFreq: 0,
+    tremSwitch: true,
+    tremFreq: 4,
     chorSwitch: false,
     synth_id: 1
 )
