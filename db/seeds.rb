30d1790e622f89396595ab1ef7915981b69350f1
jbrasko <jbrasko0508@gmail.com>
2021-02-14 12:22:42 -0600

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,39 @@
 Synth.create(title: "Simple Synth")
 
 Preset.create(
-    name: "Harsh",
+    name: "Muted Mallet",
+    waveTypeButton: 2,
+    filterDial: 300,
+    attack: 0.01,
+    decay: 1.75,
+    sustain: 0.01,
+    release: 1.75,
+    reverbDial: 0.25,
+    delayDial: 0,
+    tremSwitch: false,
+    tremFreq: 0,
+    chorSwitch: false,
+    synth_id: 1
+)
+
+Preset.create(
+    name: "Lonely Clarinet",
+    waveTypeButton: 1,
+    filterDial: 400,
+    attack: 0.25,
+    decay: 0.35,
+    sustain: 2,
+    release: 1.75,
+    reverbDial: 1,
+    delayDial: 0,
+    tremSwitch: false,
+    tremFreq: 2,
+    chorSwitch: false,
+    synth_id: 1
+)
+
+Preset.create(
+    name: "Chorus Stab",
     waveTypeButton: 3,
     filterDial: 5000,
     attack: 0.01,
@@ -25,7 +57,7 @@ Preset.create(
 )
 
 Preset.create(
-    name: "Smooth",
+    name: "Airy Tremolo",
     waveTypeButton: 0,
     filterDial: 2000,
     attack: 0.5,
@@ -33,9 +65,57 @@ Preset.create(
     sustain: 1.4,
     release: 2,
     reverbDial: 0.5,
-    delayDial: 0,
+    delayDial: 0.35,
     tremSwitch: true,
     tremFreq: 4,
+    chorSwitch: false,
+    synth_id: 1
+)
+
+Preset.create(
+    name: "Concert Hall Horn",
+    waveTypeButton: 3,
+    filterDial: 1250,
+    attack: 0.25,
+    decay: 0.6,
+    sustain: 1.5,
+    release: 1.75,
+    reverbDial: 0.85,
+    delayDial: 0.04,
+    tremSwitch: false,
+    tremFreq: 2,
+    chorSwitch: false,
+    synth_id: 1
+)
+
+Preset.create(
+    name: "Space Blip",
+    waveTypeButton: 0,
+    filterDial: 600,
+    attack: 0.01,
+    decay: 0.25,
+    sustain: 0.01,
+    release: 1.75,
+    reverbDial: 0.25,
+    delayDial: 1,
+    tremSwitch: false,
+    tremFreq: 0,
+    chorSwitch: false,
+    synth_id: 1
+)
+
+Preset.create(
+    name: "Nervous Square",
+    waveTypeButton: 1,
+    filterDial: 5000,
+    attack: 1,
+    decay: 0.25,
+    sustain: 1,
+    release: 0.25,
+    reverbDial: 0,
+    delayDial: 0,
+    tremSwitch: true,
+    tremFreq: 10,
     chorSwitch: false,
     synth_id: 1
 )
